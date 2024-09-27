d7bed27c23ba2363fb6084fe0b98138c8fd4f70d
Gnbcreator <140880653+Gnbcreator@users.noreply.github.com>
2024-09-27 21:48:13 +0530

    Update page.js

--- portfolio/frontend/src/app/components/copyrited/page.js
+++ portfolio/frontend/src/app/components/copyrited/page.js
@@ -1,4 +1,5 @@
-import { ArrowUpIcon, Button } from '@heroicons/react/24/outline'
+"use client"
+import { ArrowUpIcon } from '@heroicons/react/24/outline'
 import { CopyrightRounded } from '@mui/icons-material'
 import { Link } from 'react-scroll'
 import Container from '@mui/material/Container'
@@ -6,14 +7,10 @@ export default function Copyrited() {
   return (
     <>
       <Container maxWidth="xl" className='py-20' >
-
-
         <div className='text-center'>
-          <h1 className='text-xl'><CopyrightRounded /> copyrighted by gnbcreator</h1>
+          <h1 className='text-xl'> <CopyrightRounded /> copyrighted by gnbcreator</h1>
           <button className=' border border-blue-600 rounded-full p-2 mt-4'>
-            <Link to='/' offset={-50} smooth={10} >
-              <ArrowUpIcon className='w-7 h-7 text-yellow-300 animate-bounce' />
-            </Link>
+            <Link to='/' offset={-50} smooth={10}><ArrowUpIcon className='w-7 h-7 text-yellow-300 animate-bounce' /></Link>
           </button>
         </div>
       </Container>
